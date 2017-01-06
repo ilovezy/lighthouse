b39c501c4e0d2dc43642c3f4995213399d20de8e
Paul Irish <paul.irish@gmail.com>
2017-01-05 17:14:07 -0800

    split into its own file. errthing works.

--- lighthouse-core/lib/traces/metrics-evts.js
+++ lighthouse-core/lib/traces/metrics-evts.js
@@ -24,46 +24,52 @@ function addMetrics(traceEvents, auditResults) {
     return traceEvents;
   }
 
-  var res = auditResults;
+  const resFMPext = auditResults['first-meaningful-paint'].extendedInfo;
+  const resSIext = auditResults['speed-index-metric'].extendedInfo;
+  const resTTIext = auditResults['time-to-interactive'].extendedInfo;
 
-  const resFMP = res['first-meaningful-paint'];
-  const resFMPext = resFMP.extendedInfo;
-  const resSI = res['speed-index-metric'];
-  const resSIext = resSI.extendedInfo;
-  const resTTI = res['time-to-interactive'];
-  log.log('hi', resFMP)
-  // monotonic clock ts from the trace.
-  const navStart = resFMPext && resFMPext.value.timestamps.navStart;
-
-  const timings = [{
-    name: 'First Contentful Paint',
-    traceEvtName: 'MarkFCP',
-    value: resFMPext && (resFMPext.value.timestamps.fCP),
-  }, {
-    name: 'First Meaningful Paint',
-    traceEvtName: 'MarkFMP',
-    value: navStart + resFMP.rawValue,
-  }, {
-    name: 'Perceptual Speed Index',
-    traceEvtName: 'MarkVC50',
-    value: navStart + resSI.rawValue,
-  }, {
-    name: 'First Visual Change',
-    traceEvtName: 'MarkVC1',
-    value: resSIext && (navStart + resSIext.value.first),
-  }, {
-    name: 'Visually Complete 100%',
-    traceEvtName: 'MarkVC100',
-    value: resSIext && (navStart + resSIext.value.complete),
-  }, {
-    name: 'Time to Interactive',
-    traceEvtName: 'MarkTTI',
-    value: navStart + resTTI.rawValue,
-  }, {
-    name: 'Navigation Start',
-    traceEvtName: 'MarkNavStart',
-    value: navStart
-  }];
+  const timestamps = [
+    {
+      title: 'Navigation Start',
+      name: 'navstart',
+      ts: resFMPext.value.timestamps.navStart
+    },
+    {
+      title: 'First Contentful Paint',
+      name: 'ttfcp',
+      ts: resFMPext.value.timestamps.fCP
+    },
+    {
+      title: 'First Meaningful Paint',
+      name: 'ttfmp',
+      ts: resFMPext.value.timestamps.fMP
+    },
+    {
+      title: 'Perceptual Speed Index',
+      name: 'psi',
+      ts: resSIext.value.timestamps.perceptualSpeedIndex
+    },
+    {
+      title: 'First Visual Change',
+      name: 'fv',
+      ts: resSIext.value.timestamps.firstVisualChange
+    },
+    {
+      title: 'Visually Complete 100%',
+      name: 'vc',
+      ts: resSIext.value.timestamps.visuallyComplete
+    },
+    {
+      title: 'Time to Interactive',
+      name: 'tti',
+      ts: resTTIext.value.timestamps.timeToInteractive
+    },
+    {
+      title: 'Visually Complete 85%',
+      name: 'vc85',
+      ts: resTTIext.value.timestamps.visuallyReady
+    }
+  ];
 
   const filteredEvents = traceEvents.filter(e => {
     return e.name === 'TracingStartedInPage' || e.cat === 'blink.user_timing' || e.name === 'navigationStart';
@@ -76,26 +82,27 @@ function addMetrics(traceEvents, auditResults) {
   const navigationStartEvt = filteredEvents.filter(e => {
     return e.name === 'navigationStart' && e.pid === refEvent.pid && e.tid === refEvent.tid;
   })[0];
+  const navStartTs = timestamps.find(e => e.name === 'navstart').ts;
 
   // We are constructing performance.measure trace events, which have a start and end as follows:
   // {"pid": 89922,"tid":1295,"ts":77176783452,"ph":"b","cat":"blink.user_timing","name":"innermeasure","args":{},"tts":1257886,"id":"0xe66c67"}
   // { "pid":89922,"tid":1295,"ts":77176882592, "ph":"e", "cat":"blink.user_timing", "name":"innermeasure", "args":{ },"tts":1257898, "id":"0xe66c67" }
   let counter = (Math.random() * 1000000) | 0;
-  timings.forEach(timing => {
-    if (!timing.value || timing.value === navStart) {
+  timestamps.forEach(timing => {
+    if (!timing.ts || timing.name === 'navstart') {
       return;
     }
     const eventBase = {
-      name: timing.name,
+      name: timing.title,
       id: `0x${(counter++).toString(16)}`,
       cat: 'blink.user_timing',
     };
     const fakeMeasureStartEvent = Object.assign({}, navigationStartEvt, eventBase, {
-      ts: Math.floor(navStart * 1000),
+      ts: navStartTs,
       ph: 'b'
     });
     const fakeMeasureEndEvent = Object.assign({}, navigationStartEvt, eventBase, {
-      ts: Math.floor(timing.value * 1000),
+      ts: timing.ts,
       ph: 'e',
     });
     traceEvents.push(fakeMeasureStartEvent, fakeMeasureEndEvent);
